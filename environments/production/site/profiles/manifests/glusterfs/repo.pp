216401fc1f9d278ab8e3a958eaf01c7a215926df
Tomasz Jonik <tom.jonik@hotmail.com>
2017-07-24 00:15:59 +0100

    added missing coma

--- environments/production/site/profiles/manifests/glusterfs/repo.pp
+++ environments/production/site/profiles/manifests/glusterfs/repo.pp
@@ -11,7 +11,7 @@
 class profiles::glusterfs::repo ($version = '3.8') {
 
   tidy { '/etc/apt/sources.list.d':
-    path    => '/etc/apt/sources.list.d'
+    path    => '/etc/apt/sources.list.d',
     age     => '1w',
     matches => [ 'gluster-glusterfs-3_8*' ],
   }
