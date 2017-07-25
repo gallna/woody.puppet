115820b9c18990fdbdfa81932a6abec315484d97
Tomasz Jonik <tom.jonik@hotmail.com>
2017-07-25 03:14:50 +0100

    ffs

--- environments/production/site/profiles/manifests/glusterfs/client.pp
+++ environments/production/site/profiles/manifests/glusterfs/client.pp
@@ -24,13 +24,13 @@ class profiles::glusterfs::client ($host, $volume) {
     ensure            => absent,
     path              => '/etc/fstab',
     line              => '10.192.74.37:YogiBerra 10.192.74.37:YogiBerra glusterfs defaults,noauto,_netdev,use-readdirp=yes,direct-io-mode=disable	0	0',
-    match             => '^10.192.74.37:YogiBerra*10.192.74.37:YogiBerra',
+    match             => '^10.192.74.37:YogiBerra\s*10.192.74.37:YogiBerra',
     match_for_absence => true,
   }->
 
   mount { "/export/${volume}" :
     ensure  => 'mounted',
-    device  => "${host}:${volume}",
+    device  => "${host}:/${volume}",
     fstype  => 'glusterfs',
     options => 'defaults,noauto,_netdev,use-readdirp=yes,direct-io-mode=disable',
     atboot  => true,
