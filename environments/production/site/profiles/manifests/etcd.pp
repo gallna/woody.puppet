bde9a234d95fd41ca81a966c2386d59f2e6d111c
Tomasz Jonik <tom.jonik@hotmail.com>
2017-08-20 10:21:02 +0100

    Removed failing stuff

--- environments/production/site/profiles/manifests/etcd.pp
+++ environments/production/site/profiles/manifests/etcd.pp
@@ -3,15 +3,6 @@
 # Ensure etcd backup storage mounted
 #
 class profiles::etcd () {
-  fstab { 'Another test fstab entry':
-    ensure => absent,
-    source => 'fs-5e1bed97.efs.eu-west-1.amazonaws.com:/api/schedule-volume-io/vendor/nikic/fast-route/route/efs-schedule/etcd',
-    dest   => '/var/etcd/backups',
-    type   => 'nfs4',
-    opts   => 'nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,_netdev',
-    dump   => 0,
-    passno => 0,
-  }
   file { ['/var/etcd', '/var/etcd/backups']:
     ensure => 'directory',
     owner  => 'root',
